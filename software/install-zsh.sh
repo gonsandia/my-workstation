ab0ffd728a3c31c1823883d7c5551ca84bbc87fc
gonzalo <gonzalosantos89@gmail.com>
2021-09-28 10:27:38 +0200

    removed completions

--- software/install-zsh.sh
+++ software/install-zsh.sh
@@ -42,7 +42,6 @@ if ! [ -a "~/.zshrc" ]; then
 
     cd ~/.oh-my-zsh/custom/plugins
     git clone https://github.com/zsh-users/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-completions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
     git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins autoupdate
     git clone https://github.com/romkatv/powerlevel10k.git
@@ -50,7 +49,7 @@ if ! [ -a "~/.zshrc" ]; then
     cd ${SETUP_DIR}
 
     sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" ~/.zshrc
-    sed -i "s/plugins=(git)/plugins=(\n  git\n  zsh-completions\n  zsh-syntax-highlighting\n  autoupdate\n  zsh-autosuggestions\n)/g" ~/.zshrc
+    sed -i "s/plugins=(git)/plugins=(\n  git\n  zsh-syntax-highlighting\n  autoupdate\n  zsh-autosuggestions\n)/g" ~/.zshrc
 
 #    echo "POWERLEVEL9K_MODE="nerdfont-complete"" >> ~/.zshrc
 #    echo "" >> ~/.zshrc
